d0537aa2c5c74d06cbfb78348c3a626d0508f9bf
Pierre Fils <epierrefils@gmail.com>
2023-04-05 23:06:04 -0400

    Plus Minus Interview Prep Kits Java

---
+++ src/Week1/PlusMinus.java
@@ -0,0 +1,55 @@
+package Week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        int n = arr.size();
+        int positiveCount = 0, negativeCount = 0, zeroCount = 0;
+        for (int i = 0; i < n; i++) {
+            int num = arr.get(i);
+            if (num > 0) {
+                positiveCount++;
+            } else if (num < 0) {
+                negativeCount++;
+            } else {
+                zeroCount++;
+            }
+        }
+        double positiveRatio = (double) positiveCount / n;
+        double negativeRatio = (double) negativeCount / n;
+        double zeroRatio = (double) zeroCount / n;
+        System.out.printf("%.6f\n%.6f\n%.6f\n", positiveRatio, negativeRatio, zeroRatio);
+    }
+
+}
+
+public class PlusMinus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
