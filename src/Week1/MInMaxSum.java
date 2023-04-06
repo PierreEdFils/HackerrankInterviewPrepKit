d4df35f4a9304992513063e395d4552a5a0f1d26
Pierre Fils <epierrefils@gmail.com>
2023-04-05 23:11:21 -0400

    Min Max Sum Interview Prep Kits Java

---
+++ src/Week1/MInMaxSum.java
@@ -0,0 +1,47 @@
+package Week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result1 {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        long sum = 0, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.size(); i++) {
+            int num = arr.get(i);
+            sum += num;
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+        long maxSum = sum - min;
+        long minSum = sum - max;
+        System.out.printf("%d %d", minSum, maxSum);
+    }
+
+}
+
+public class MInMaxSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result1.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
