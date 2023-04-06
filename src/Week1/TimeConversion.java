7149a49a4910716a9b2d1ef67cc9990a11c35fdf
Pierre Fils <epierrefils@gmail.com>
2023-04-05 23:14:57 -0400

    Time Conversion Interview Prep Kits Java

---
+++ src/Week1/TimeConversion.java
@@ -0,0 +1,49 @@
+package Week1;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class Result2 {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+        // Write your code here
+        DateFormat inputFormat = new SimpleDateFormat("hh:mm:ssaa");
+        DateFormat outputFormat = new SimpleDateFormat("HH:mm:ss");
+        try {
+            Date date = inputFormat.parse(s);
+            return outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+}
+
+public class TimeConversion {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result2.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
