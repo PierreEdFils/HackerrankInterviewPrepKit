358af735f91080532ada0cd864e32b2eaaa4baba
Pierre Fils <epierrefils@gmail.com>
2023-04-07 22:07:36 -0400

    Day 3 Flippings bits Prep Kits Java

---
+++ src/Week1/Day3/Flippingbits.java
@@ -0,0 +1,46 @@
+package Week1.Day3;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+class Result3 {
+
+    /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long flippingBits(long n) {
+        // Write your code here
+        long maxUnsignedInt = (1L << 32) - 1;
+        return n ^ maxUnsignedInt;
+
+    }
+
+}
+public class Flippingbits {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = Result3.flippingBits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
